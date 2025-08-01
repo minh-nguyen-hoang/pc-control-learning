3b8d68efa756695f1a5fb55edf004bf8a4be00bd
Minh Nguyen <minh.anm.nguyen@gmail.com>
2025-08-01 14:34:09 +0700

    Add bai1 exercise
    Signed-off-by: Minh Nguyen <minh.anm.nguyen@gmail.com>

---
+++ bai1_1_8_2025/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System.ComponentModel.Design;
+
+
+/*
+ *  Bài 1. Đề bài: Đầu vào là các kí tự chữ cái (a->z, A->Z), Đầu ra mong muốn như ví dụ sau:
+    Ví dụ: 
+    Input: aaabbc
+    Output: a3b2c1
+    Input: ggggbkkc
+    Output:g4b1k2c
+ */
+
+Console.Write("Enter string: ");
+string str = Console.ReadLine();
+
+int right_pos = 0, left_pos = 0;
+string ret = "";
+
+while (right_pos != str.Length)
+{
+    if (str[right_pos] != str[left_pos])
+    {
+        ret += str[left_pos] + Convert.ToString(right_pos - left_pos);
+
+        left_pos = right_pos;
+    }
+
+    ++right_pos;
+}
+
+ret += str[left_pos] + Convert.ToString(right_pos - left_pos);
+
+
+Console.Write("Result: " + ret);
